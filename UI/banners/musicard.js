c6882465ed47442e7ddec0e8d3bb5da9c9e41b7f
Kurai002 <viemnam231@gmail.com>
2024-09-08 18:41:52 +0700

    Update musicard.js

--- UI/banners/musicard.js
+++ UI/banners/musicard.js
@@ -4,7 +4,7 @@ const data = {
     backgroundImages: [
         path.resolve(__dirname, '../musicimages/1.png'),
         path.resolve(__dirname, '../musicimages/2.jpg'),
-        path.resolve(__dirname, '../musicimages/3.jpg'),
+        path.resolve(__dirname, '../musicimages/6.jpg'),
         path.resolve(__dirname, '../musicimages/4.jpg'),
         path.resolve(__dirname, '../musicimages/5.jpg'),
     ],
